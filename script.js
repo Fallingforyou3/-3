8ccd8c6d1fbf0c3713531d870b6ab976f451c48e
Fallingforyou3 <jayardelrosario1234567@gmail.com>
2025-02-07 16:43:21 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 function checkAnswer() {
-    const correctAnswer = "wave";  // Change this to your desired answer
+    const correctAnswer = "mikey";  // Change this to your desired answer
     const userInput = document.getElementById("answerInput").value.trim().toLowerCase();
 
     if (userInput === correctAnswer) {
-        window.location.href = "https://sendthesong.xyz/";  // Change to your desired URL
+        window.location.href = "pornhub.com";  // Change to your desired URL
     } else {
         alert("Incorrect answer. Try again.");
     }
