a02c28562929c16ecb0ae73ae6e0bc3f97e89a6d
Fallingforyou3 <jayardelrosario1234567@gmail.com>
2025-02-07 17:30:53 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 function checkAnswer() {
-    const correctAnswer = "mikey";  // Change this to your desired answer
+    const correctAnswer = "the 1975";  // Change this to your desired answer
     const userInput = document.getElementById("answerInput").value.trim().toLowerCase();
 
     if (userInput === correctAnswer) {
-        window.location.href = "https://www.pornhub.com/";  // Change to your desired URL
+        window.location.href = "https://sendthesong.xyz/";  // Change to your desired URL
     } else {
         alert("Incorrect answer. Try again.");
     }
