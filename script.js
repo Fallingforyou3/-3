d27fad5578571c97095d8e604c624907bc04975c
Fallingforyou3 <jayardelrosario1234567@gmail.com>
2025-02-08 23:47:45 +0800

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function checkAnswer() {
     const userInput = document.getElementById("answerInput").value.trim().toLowerCase();
 
     if (userInput === correctAnswer) {
-        window.location.href = "https://sendthesong.xyz/details/67a738181fc54584f656be0b";  // Change to your desired URL
+        window.location.href = "https://sendthesong.xyz/details/67a77c391fc54584f656d3c3";  // Change to your desired URL
     } else {
         alert("Incorrect answer. Try again.");
     }
