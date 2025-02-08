31f53ad86bec2ef32a2ef8f200acdff6ec4dfcdc
Fallingforyou3 <jayardelrosario1234567@gmail.com>
2025-02-08 18:56:38 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 function checkAnswer() {
-    const correctAnswer = "the 1975";  // Change this to your desired answer
+    const correctAnswer = "wave";  // Change this to your desired answer
     const userInput = document.getElementById("answerInput").value.trim().toLowerCase();
 
     if (userInput === correctAnswer) {
-        window.location.href = "https://sendthesong.xyz/details/67a5d1cd1fc54584f6567ab6";  // Change to your desired URL
+        window.location.href = "https://sendthesong.xyz/details/67a738181fc54584f656be0b";  // Change to your desired URL
     } else {
         alert("Incorrect answer. Try again.");
     }
