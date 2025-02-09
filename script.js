881dccf41bf913438dd626b58a3a4cb7a919308e
Fallingforyou3 <jayardelrosario1234567@gmail.com>
2025-02-09 19:03:25 +0800

    pdate script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function checkAnswer() {
     const userInput = document.getElementById("answerInput").value.trim().toLowerCase();
 
     if (userInput === correctAnswer) {
-        window.location.href = "https://sendthesong.xyz/details/67a889d51fc54584f6570415";  // Change to your desired URL
+        window.location.href = "https://sendthesong.xyz/details/67a88b5d1fc54584f6570482";  // Change to your desired URL
     } else {
         alert("Incorrect answer. Try again.");
     }
